b016a35367e889a2e9883d9858de93540672b3e2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:32 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+data "azurerm_key_vault_secret" "cosmosdb_account_key" {
+  for_each     = { for k, v in var.stream_analytics_output_cosmosdbs : k => v if v.cosmosdb_account_key_key_vault_id != null && v.cosmosdb_account_key_key_vault_secret_name != null }
+  name         = each.value.cosmosdb_account_key_key_vault_secret_name
+  key_vault_id = each.value.cosmosdb_account_key_key_vault_id
+}
 resource "azurerm_stream_analytics_output_cosmosdb" "stream_analytics_output_cosmosdbs" {
   for_each = var.stream_analytics_output_cosmosdbs
 
   container_name           = each.value.container_name
-  cosmosdb_account_key     = each.value.cosmosdb_account_key
+  cosmosdb_account_key     = each.value.cosmosdb_account_key != null ? each.value.cosmosdb_account_key : try(data.azurerm_key_vault_secret.cosmosdb_account_key[each.key].value, null)
   cosmosdb_sql_database_id = each.value.cosmosdb_sql_database_id
   name                     = each.value.name
   stream_analytics_job_id  = each.value.stream_analytics_job_id
--- variables.tf
+++ variables.tf
@@ -4,6 +4,8 @@ Map of stream_analytics_output_cosmosdbs, attributes below
 Required:
     - container_name
     - cosmosdb_account_key
+    - cosmosdb_account_key_key_vault_id (alternative to cosmosdb_account_key - read from Key Vault instead)
+    - cosmosdb_account_key_key_vault_secret_name (alternative to cosmosdb_account_key - read from Key Vault instead)
     - cosmosdb_sql_database_id
     - name
     - stream_analytics_job_id
@@ -14,14 +16,16 @@ Optional:
 EOT
 
   type = map(object({
-    container_name           = string
-    cosmosdb_account_key     = string
-    cosmosdb_sql_database_id = string
-    name                     = string
-    stream_analytics_job_id  = string
-    authentication_mode      = optional(string) # Default: "ConnectionString"
-    document_id              = optional(string)
-    partition_key            = optional(string)
+    container_name                             = string
+    cosmosdb_account_key                       = string
+    cosmosdb_account_key_key_vault_id          = optional(string)
+    cosmosdb_account_key_key_vault_secret_name = optional(string)
+    cosmosdb_sql_database_id                   = string
+    name                                       = string
+    stream_analytics_job_id                    = string
+    authentication_mode                        = optional(string) # Default: "ConnectionString"
+    document_id                                = optional(string)
+    partition_key                              = optional(string)
   }))
 }
 
