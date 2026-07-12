29f8af535f68ee0d20eda5cb9e995a358864eff3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "stream_analytics_output_cosmosdbs_id" {
+  description = "Map of id values across all stream_analytics_output_cosmosdbs, keyed the same as var.stream_analytics_output_cosmosdbs"
+  value       = { for k, v in azurerm_stream_analytics_output_cosmosdb.stream_analytics_output_cosmosdbs : k => v.id }
+}
 output "stream_analytics_output_cosmosdbs_authentication_mode" {
   description = "Map of authentication_mode values across all stream_analytics_output_cosmosdbs, keyed the same as var.stream_analytics_output_cosmosdbs"
   value       = { for k, v in azurerm_stream_analytics_output_cosmosdb.stream_analytics_output_cosmosdbs : k => v.authentication_mode }
